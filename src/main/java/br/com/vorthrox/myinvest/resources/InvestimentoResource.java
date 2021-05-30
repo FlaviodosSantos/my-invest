1125bfeab8e53041280e6f7b370aab0c0cd01b47
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-05-30 19:26:46 -0300

    categoriaResource finalizado

--- src/main/java/br/com/vorthrox/myinvest/resources/InvestimentoResource.java
+++ src/main/java/br/com/vorthrox/myinvest/resources/InvestimentoResource.java
@@ -3,6 +3,7 @@ package br.com.vorthrox.myinvest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import br.com.vorthrox.myinvest.repository.InvestimentoRepository;
 
 @RestController
 @RequestMapping("/investimentos")
+@CrossOrigin(origins = "http://localhost:3000")
 public class InvestimentoResource {
 
 	@Autowired
